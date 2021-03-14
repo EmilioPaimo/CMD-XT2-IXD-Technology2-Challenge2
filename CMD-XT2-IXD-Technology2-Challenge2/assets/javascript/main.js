5e1dafe0b598aaab1f115e03d07b74f71a802cd9
EmilioPaimo <37639342+EmilioPaimo@users.noreply.github.com>
2021-03-15 00:51:21 +0100

    Add files via upload

---
+++ CMD-XT2-IXD-Technology2-Challenge2/assets/javascript/main.js
@@ -0,0 +1,45 @@
+// week count
+var daysofweek = ['sun', 'mon', 'tus', 'wed', 'thu', 'fri', 'sat'];
+// mount count
+var month = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+
+function clock() {
+    // setting up variabele
+    var today = new Date(); // date counts
+    var h = today.getHours(); // hours count 
+    var m = today.getMinutes(); // minutes count
+    var s = today.getSeconds(); // seconds count
+    var day = h < 11 ? 'AM' : 'PM'; // AM or PM count
+    var daytoday = today.getDay()
+    var date = today.getDate(); // date count 
+    var mon = today.getMonth(); // month count
+    var year = today.getFullYear(); // Year count
+
+
+    // adding leading zeros to them
+
+    h = h < 10 ? '0' + h : h;
+    m = m < 10 ? '0' + m : m;
+    s = s < 10 ? '0' + s : s;
+
+
+    // writing it down in the document
+    document.getElementById('hours').innerHTML = h;
+    document.getElementById('min').innerHTML = m;
+    document.getElementById('sec').innerHTML = s;
+    document.getElementById('time').innerHTML = day;
+    document.getElementById('day').innerHTML = date;
+    document.getElementById('month').innerHTML = month[mon];
+    document.getElementById('year').innerHTML = year;
+    document.getElementById('' + daysofweek[daytoday] + '').style.color = '#fff';
+
+}
+var inter = setInterval(clock, 400); // End Javascript
+
+// button
+const input = document.querySelector('input[type="checkbox"]');
+function handleInput() {
+    const { checked } = this;
+    document.querySelector('body').style.background = checked ? '#151d29' : '#d6e7f7';
+}
+input.addEventListener('input', handleInput);
